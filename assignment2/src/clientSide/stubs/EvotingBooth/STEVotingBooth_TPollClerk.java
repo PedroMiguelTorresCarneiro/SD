80c588b97757d1d75f0ea6839359707a50ec6a1e
PedroMiguelTorresCarneiro <pedrocarneiro@ua.pt>
2025-05-05 16:55:43 +0100

    add stub to logic

--- assignment2/src/clientSide/stubs/EvotingBooth/STEVotingBooth_TPollClerk.java
+++ assignment2/src/clientSide/stubs/EvotingBooth/STEVotingBooth_TPollClerk.java
@@ -8,21 +8,60 @@ package clientSide.stubs.EvotingBooth;
  * @author Inês Águia
  * @author Pedro Carneiro
  */
-public interface STEVotingBooth_TPollClerk {
+public class STEVotingBooth_TPollClerk {
+    private static STEVotingBooth_TPollClerk instance = null;
+    private final String serverHost;
+    private final int serverPort;
+
+    private STEVotingBooth_TPollClerk(String host, int port){
+        this.serverHost = host;
+        this.serverPort = port;
+    }
+
+    public STEVotingBooth_TPollClerk getInstance(String host, int port){
+        if (instance == null) {
+            instance = new STEVotingBooth_TPollClerk(host, port);
+        }
+        return instance;
+    }
+    
     /**
      * The gathering method is called by the poll clerk to gather all the votes.
      * @throws InterruptedException if the thread is interrupted.
      */
-    void gathering() throws InterruptedException;
+    public void gathering() throws InterruptedException{
+        ClientCom com;                                                 
+        Message outMessage, inMessage; 
+
+        com = new ClientCom(serverHost, serverPort);
+        /*
+            logic to handle the connection to the server
+            and the response from the server
+        */
+
+        outMessage = new Message(MessageType.GATHERING, null);
+        
+        com.writeObject(outMessage);
+        inMessage = (Message) com.readObject();
+        /*
+            logic to handle the response from the server
+        */
+
+        com.close();
+    };
     
     /**
      * The publishElectionResults method is called by the poll clerk to publish the election results.
      */
-    void publishElectionResults();
+    public void publishElectionResults(){
+
+    };
     
     /**
      * The getVotesCount method is called by the poll clerk to get the number of voters.
      * @return the number of voters that have voted.
      */
-    int getVotesCount();
+    public int getVotesCount(){
+
+    };
 }
