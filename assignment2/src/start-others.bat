9adb5f57a3bd43b42c163dca4eaadf1bc088485e
davidpalricas <davipalricas22@gmail.com>
2025-05-10 20:35:00 +0100

    Updates

--- assignment2/src/start-others.bat
+++ assignment2/src/start-others.bat
@@ -1,22 +1,26 @@
 @echo off
-setlocal enabledelayedexpansion
-
-set "SRC_DIR=%~dp0"
-set "BUILD_DIR=%SRC_DIR%..\build"
-
 echo 🚀 A iniciar servidores restantes...
 
-start "SIDCheck" cmd /k "cd /d \"%SRC_DIR%\" && java -cp \"%BUILD_DIR%\" serverSide.main.SIDCheck"
+:: Define o diretório de origem
+set SRC_DIR=%~dp0
+set BUILD_DIR=%SRC_DIR%..\build
+
+:: Inicia os servidores em janelas separadas
+start "SIDCheck" cmd /k "cd /d "%SRC_DIR%" && java -cp "%BUILD_DIR%" serverSide.main.SIDCheck"
 timeout /t 1 >nul
-start "SPollStation" cmd /k "cd /d \"%SRC_DIR%\" && java -cp \"%BUILD_DIR%\" serverSide.main.SPollStation"
+start "SPollStation" cmd /k "cd /d "%SRC_DIR%" && java -cp "%BUILD_DIR%" serverSide.main.SPollStation"
 timeout /t 1 >nul
-start "SEvotingBooth" cmd /k "cd /d \"%SRC_DIR%\" && java -cp \"%BUILD_DIR%\" serverSide.main.SEvotingBooth"
+start "SEvotingBooth" cmd /k "cd /d "%SRC_DIR%" && java -cp "%BUILD_DIR%" serverSide.main.SEvotingBooth"
 timeout /t 1 >nul
-start "SExitPoll" cmd /k "cd /d \"%SRC_DIR%\" && java -cp \"%BUILD_DIR%\" serverSide.main.SExitPoll"
+start "SExitPoll" cmd /k "cd /d "%SRC_DIR%" && java -cp "%BUILD_DIR%" serverSide.main.SExitPoll"
 timeout /t 1 >nul
 
-start "CPollClerk" cmd /k "cd /d \"%SRC_DIR%\" && java -cp \"%BUILD_DIR%\" clientSide.main.CPollClerk"
+:: Inicia os clientes em janelas separadas
+start "CVoter" cmd /k "cd /d "%SRC_DIR%" && java -cp "%BUILD_DIR%" clientSide.main.CVoter"
 timeout /t 1 >nul
-start "CPollster" cmd /k "cd /d \"%SRC_DIR%\" && java -cp \"%BUILD_DIR%\" clientSide.main.CPollster"
+start "CPollClerk" cmd /k "cd /d "%SRC_DIR%" && java -cp "%BUILD_DIR%" clientSide.main.CPollClerk"
 timeout /t 1 >nul
-start "CVoter" cmd /k "cd /d \"%SRC_DIR%\" && java -cp \"%BUILD_DIR%\" clientSide.main.CVoter"
+start "CPollster" cmd /k "cd /d "%SRC_DIR%" && java -cp "%BUILD_DIR%" clientSide.main.CPollster"
+timeout /t 1 >nul
+
+pause
